e4a42d4ccbbcaa0ce67b2b0355355de38351b01e
Divyansh Verma <174802779+justdivy@users.noreply.github.com>
2026-08-07 15:51:35 +0530

    Time: 0 ms (100%), Space: 44 MB (16.81%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+
+        for(int i = 0; i<rows; i++){
+            for(int j = i; j<cols; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for(int i=0; i<rows; i++){
+
+            int stCols=0; 
+            int endCols = cols - 1;
+
+            while(stCols < endCols){
+                int temp = arr[i][stCols];
+                arr[i][stCols] = arr[i][endCols];
+                arr[i][endCols] = temp;
+
+                stCols++;
+                endCols--;
+            }
+        }
+    }
+}
